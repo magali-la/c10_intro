5ab8b5ee902839346476eb81de3a72d5c7d3f6e5
Magali L <magali.lapu@gmail.com>
2024-07-23 23:56:14 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+let name = "Mala";
+
+function greet() {
+    alert ("Welcome to my website");
+
+}
+
+let locate = "Location"
+let age = 50;
+let hasEaten = false;
+
+let items = [25, 45, "food", "abuja", [12, "laptop"], true];
+
+
+let student = {
+    firstname: "Mala",
+    age: 75,
+    state: "USA",
+
+};
+console.log(student.firstname)
+
+function greetUser() {
+    alert("Welcome user"),
+}
