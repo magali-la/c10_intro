4d905e6b39db02e4ba7cff6e167c2642febc9767
Magali L <magali.lapu@gmail.com>
2024-08-19 10:36:17 +0200

    added visible log and format css

--- src/index.js
+++ src/index.js
@@ -1,25 +1,89 @@
-let name = "Mala";
 
-function greet() {
-    alert ("Welcome to my website");
+let buttonOnPage = document.getElementById ("btn");
 
-}
+buttonOnPage.addEventListener("click", () => {
+    let bgColor = generateBG();
+    document.body.style.backgroundColor = bgColor;
+    colorLog.push(bgColor);
+    console.log(colorLog);
 
-let locate = "Location"
-let age = 50;
-let hasEaten = false;
+    const listElement = document.getElementById("myLogs")
+    listElement.innerHTML = ''
+    colorLog.forEach(color => {
+        const listItem = document.createElement('li')
+        listItem.textContent = color
+        listElement.appendChild(listItem)
+    })
+});
 
-let items = [25, 45, "food", "abuja", [12, "laptop"], true];
-
-
-let student = {
-    firstname: "Mala",
-    age: 75,
-    state: "USA",
-
+function generateBG() {
+    return arr[(Math.floor(Math.random() * arr.length))]
 };
-console.log(student.firstname)
 
-function greetUser() {
-    alert("Welcome user"),
-}
+let arr = ['#B0DB43', '#12EAEA', '#BCE7FD', '#FF5733', '#FF33F6', '#F3FF33', '#FF8C33','#CAD178', '#754F5B', '#1C448E', '#6461A0', '#DE4D86', '#F29CA3', '#F29CA3', '#FA7E61', '#F00699', '#018E42', '#F7D002', '#81E979', '#98473E', '#754668'];
+
+let colorLog = [];
+
+
+
+
+
+// function generateColor() {
+//     let color = '#';
+//     for(let i=0; i < 6; i++) {
+//         color += randomColor[Math.floor(Math.random() * 16)];
+//         console.log(color);
+
+//     }
+//     return color;
+// }
+
+// buttonOnPage.addEventListener ('click', ()=> {
+//     let bgColor = generateColor();
+//     document.body.style.backgroundColor = bgColor;
+
+
+// });
+
+// end of functioning random generator
+
+
+// let buttonOnPage = document.getElementById ("btn");
+
+// let colors =['#005F74']
+// let colors = ['#F09BG2']
+
+// buttonOnPage.addEventListener ('click', ()=> {
+//         const random += "#" + Math.floor(Math.random() * 10 )
+//         let backgroundColor = generateColor();
+//         document.body.style.backgroundColor = bgColor;
+
+// }
+
+// buttonOnPage.addEventListener('click',console.log("test"))
+
+// uran
+
+// let arr = ['#B0DB43', '#12EAEA', '#BCE7FD'];
+
+// for (let i=0; i < arr.length; i++) {
+// console.log (randomColor);
+//     colors.push(randomColor);
+
+// }
+
+// arr.map((item)=> {});
+
+
+
+// function generateColor () {
+
+//     // choose random color from arr
+//     const totalColors = arr.length
+//     const randomIndex = Math.floor(Math.random()*3)
+
+//     document.body.style.backgroundColor = arr[randomIndex];
+
+
+// }
+
